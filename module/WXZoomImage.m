8750c7aed0db902f179fcbb984f9569b40a54867
郑江荣 <362675035@qq.com>
2019-06-07 17:43:17 +0800

    update

--- module/WXZoomImage.m
+++ module/WXZoomImage.m
@@ -8,6 +8,7 @@
 #import "WXZoomImage.h"
 #import "LPPhotoViewer.h"
 #import "farwolf.h"
+#import "farwolf_weex.h"
 #import <WeexPluginLoader/WeexPluginLoader.h>
 WX_PlUGIN_EXPORT_MODULE(zoomImage, WXZoomImage)
 @implementation WXZoomImage
@@ -21,7 +22,15 @@ WX_EXPORT_METHOD(@selector(dismiss))
     if([p objectForKey:@"index"])
         index=[@"" add:p[@"index"]].intValue;
     LPPhotoViewer *pvc = [[LPPhotoViewer alloc] init];
-    pvc.imgArr = array;
+    NSMutableArray *ary=[NSMutableArray new];
+    for(NSString *url in array){
+        if([url startWith:PREFIX_SDCARD]){
+            [ary addObject: [url replace:PREFIX_SDCARD withString:@""]];
+        }else{
+             [ary addObject: url];
+        }
+    }
+    pvc.imgArr = ary;
     pvc.currentIndex = index;
     pvc.indicatorType = IndicatorTypeNumLabel;
     [self.weexInstance.viewController.TopViewController presentViewController:pvc animated:YES completion:nil];
